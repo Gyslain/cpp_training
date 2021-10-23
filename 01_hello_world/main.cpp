d4b917e02eeb9a0e57ec7c22a1010fc9271e9fb8
Ghislain Viguier <ghislain.viguier@gmail.com>
2021-10-23 19:39:56 +0200

    added comments

--- 01_hello_world/main.cpp
+++ 01_hello_world/main.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
+#include <iostream> // insertion de iostream pour l'utilisationde cout et endl
 
-using namespace std;
+using namespace std; // utilisation de l'espace de nom standard
 
 int main(int argc, char **argv)
 {
